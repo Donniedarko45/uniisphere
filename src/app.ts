422625c3671d490274859dca6185d392f7f14fa9
Kartikey Pandey <kartikey224252@gmail.com>
2025-01-31 11:46:33 +0530

    updating cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import http from "http";
+import cors from "cors";
 import { setupSocket } from "./utils/socket";
 import authRoutes from "./routes/auth.routes";
 import postRoutes from "./routes/post.routes";
 import { getProfile } from "./controllers/user.controller";
 dotenv.config();
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
 const server = http.createServer(app);
 const io = setupSocket(server); // we have to pass this to socket setup
 server.listen(process.env.PORT || 5000, () => {
