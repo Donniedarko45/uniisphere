291aeb9b78a3469da910105fc10e8d2cac89708c
Kartikey Pandey <kartikey224252@gmail.com>
2025-04-02 05:15:52 +0530

    fixed accept connection request

--- src/routes/connection.routes.ts
+++ src/routes/connection.routes.ts
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
 import {
-	acceptConnection,
-	declineConnection,
-	getConnections,
-	getConnectionStats,
-	getPendingRequests,
-	sendConnectionRequest
+  acceptConnection,
+  declineConnection,
+  getConnections,
+  getConnectionStats,
+  getPendingRequests,
+  sendConnectionRequest
 } from "../controllers/connection.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 router.post("/connect/:userId", authenticate, sendConnectionRequest);
 router.post('/accept/:connectionId', authenticate, acceptConnection);
-router.post('/getPending/:userId',authenticate,getPendingRequests);
+router.post('/getPending/', authenticate, getPendingRequests);
 router.post('/decline/:connectionId', authenticate, declineConnection);
 router.get('/connections', authenticate, getConnections);
 router.get('/stats/:userId', authenticate, getConnectionStats); // New endpoint
